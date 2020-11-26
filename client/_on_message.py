04aa1898db48f71dc8747baa6af72efcfed51ff7
Creonalia <hyper.pyromaniac@simplelogin.co>
2020-11-25 21:46:21 -0800

    clean up on_message

--- client/_on_message.py
+++ client/_on_message.py
@@ -1,33 +1,32 @@
-from datetime import datetime as dt
+from datetime import datetime
 from client.server_helper import ServerHelper
 
 
 async def _on_message(self, message):
     if message.guild.id not in self.s:
         self.s[message.guild.id] = ServerHelper()
-    clippedMessage = message.content if len(message.content) < 64 else message.content[:64]
-    print(str(message.author).ljust(32) + "> " + clippedMessage + " " + str(dt.now()))
-
-    text = message.content
+    clipped_message = message.content if len(message.content) < 64 else message.content[:64]
+    print(f"{str(message.author).ljust(32)}> {clipped_message} {datetime.now()}")
 
     if message.author == self.user:
         return
 
+    text = message.content.lower().strip()
+
     # Do commands!
-    if text.lower().strip().startswith(f"{self.prefix}q"):
-        await self.send_question(message)
-        return
+    if text.startswith(self.prefix):
+        text = text[1:]
+        if text.startswith("q"):
+            await self.send_question(message)
 
-    if text.lower().strip().startswith(f"{self.prefix}a"):
-        await self.answer_question(message)
-        return
+        elif text.startswith("a"):
+            await self.answer_question(message)
 
-    if text.lower().strip() == f"{self.prefix}points":
-        await self.query_points(message)
-        return
+        elif text == "points":
+            await self.query_points(message)
 
-    if text.lower().strip() == f"{self.prefix}help":
-        await self.send_help_text(message)
-        return
+        elif text == "help":
+            await self.send_help_text(message)
 
-    await self.ping(message)
+    else:
+        await self.ping(message)

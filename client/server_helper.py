05d8d2236ed172c4cf9ea2be01fc10932ba20ac8
William Y. Feng <williamfeng1729@gmail.com>
2021-02-21 09:55:25 -0800

    Fix more bugs
    uhhhhh fixing conflicts I guess

--- client/server_helper.py
+++ client/server_helper.py
@@ -5,6 +5,5 @@ class ServerHelper:
     def __init__(self):
         self.question = collections.defaultdict(dict)
         self.answers = collections.defaultdict(str)
-        self.answered = collections.defaultdict(lambda: True)
         self.last_sent_question = collections.defaultdict(int)
         self.channel_to_cat = collections.defaultdict(str)
